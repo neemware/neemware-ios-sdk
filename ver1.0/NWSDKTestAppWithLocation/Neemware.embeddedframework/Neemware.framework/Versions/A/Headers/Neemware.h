f651ce3ff15c3b6d4f93e4277635a617d99d368f
Mads Srinivasan <mads@neemware.com>
2012-10-23 19:14:46 -0700

    Fixed iPad orientation bug

--- ver1.0/NWSDKTestAppWithLocation/Neemware.embeddedframework/Neemware.framework/Versions/A/Headers/Neemware.h
+++ ver1.0/NWSDKTestAppWithLocation/Neemware.embeddedframework/Neemware.framework/Versions/A/Headers/Neemware.h
@@ -14,7 +14,8 @@
 }
 
 /**
- * This init method must be called before using Neemware SDK using key/secret from Neemware website
+ * This method must be called before using the Neemware SDK
+ * sing key/secret from Neemware website
  * This method should only be called once, typically in application:didFinishLaunchingWithOptions
  * This method must be called before any other Neemware methods are called
  */
@@ -26,6 +27,7 @@
 
 // Call this method whenever new location information is obtained
 // For example, call this method in your CLLocationManagerDelegate's didUpdateToLocation:FromLocation:
+// and that location will be sent to Neemware on subsequent calls or updates
 + (void)setUserLocationWithLatitude:(double)lat andLongitude:(double)lon;
 
 /**
@@ -41,7 +43,7 @@
 
 ////////////////////////////////////////////////////////////
 
-// This section for inbox-specific things
+// Inbox-related methods
 //
 //  Accessors
 //
@@ -53,9 +55,15 @@
 // Typical usage within a UIViewController will be [Neemware displayInboxFrom:self];
 + (void)            displayInboxFrom:(UIViewController *)vc;
 
+// For more options, or for displaying on an iPad
++ (void)            displayInboxFrom:(UIViewController *)vc
+                        withModalTransitionStyle:(UIModalTransitionStyle)ts
+                            modalPresentationStyle:(UIModalPresentationStyle)ps
+                                animation:(BOOL)animated;
+
 ////////////////////////////////////////////////////////////
 
-// This section for banner-specific things
+// For displaying the banner and subsequent UIViewController
 //
 //  Methods
 //
@@ -63,10 +71,14 @@
 // would like to be available to display banner messages.  Banners will only be displayed
 // when you select 'Display in Banner' in the Neemware Web Dashboard so it is up to you when
 // it is displayed or not
-+(void)showBannerInViewController:(UIViewController *)vController;
-+(void)showBannerInViewController:(UIViewController *)vController withDelay:(NSInteger)delay;
-/////////////
-// Feedback stuff
-//
-+(void)showFeedbackFormIn:(UIViewController *)vController;
++ (void)showBannerInViewController:(UIViewController *)vController;
+
+// When the banner is tapped, a UIViewController will be presented
+// This optional method (**recommended for iPads**) provides control over how that
+// UIViewController is presented (delay, however, refers to the delay in displaying the banner itself)
++ (void)showBannerInViewController:(UIViewController *)vController
+                            withDelay:(NSInteger)delay
+                                modalTransitionStyle:(UIModalTransitionStyle)ts
+                                    modalPresentationStyle:(UIModalPresentationStyle)ps
+                                        animation:(BOOL)animated;
 @end
